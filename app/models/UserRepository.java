de6afcdc52c9d3c58879a8901cc3de85981031c2
s-santoro <ssantoro@ntb.ch>
2018-12-13 21:40:33 +0100

    login- and signup-validation done, userlogin returns status 200 or 403

--- app/models/UserRepository.java
+++ app/models/UserRepository.java
@@ -1,6 +1,7 @@
 package models;
 
 import com.google.inject.Inject;
+import play.Logger;
 import play.db.jpa.JPAApi;
 
 import javax.persistence.EntityManager;
@@ -16,90 +17,112 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
  * This class interacts with the Postgresql-Database by utilizing JPA and EntityManager API.
  * It creates another layer(repository-pattern) for accessing the database.
  */
-public class UserRepository {
+public class UserRepository
+{
 
-    private final JPAApi jpaApi;
-    private final SessionRepository sessionRepository;
+	private final JPAApi jpaApi;
+	private final SessionRepository sessionRepository;
 
-    @Inject
-    public UserRepository(JPAApi jpaApi, SessionRepository sessionRepository) {
-        this.sessionRepository = sessionRepository;
-        this.jpaApi = jpaApi;
-    }
+	@Inject public UserRepository(JPAApi jpaApi,
+			SessionRepository sessionRepository)
+	{
+		this.sessionRepository = sessionRepository;
+		this.jpaApi = jpaApi;
+	}
 
-    public CompletionStage<User> add(User user) {
+	public CompletionStage<User> add(User user)
+	{
 
-        return supplyAsync(() -> wrap(em -> insert(em, user)));
-    }
+		return supplyAsync(() -> wrap(em -> insert(em, user)));
+	}
 
-    public CompletionStage<Stream<User>> findAll() {
+	public CompletionStage<Stream<User>> findAll()
+	{
 
-        return supplyAsync(() -> wrap(em -> findAll(em)));
-    }
+		return supplyAsync(() -> wrap(em -> findAll(em)));
+	}
 
-    public CompletionStage<User> find(Long id) {
+	public CompletionStage<User> find(Long id)
+	{
 
-        return supplyAsync(() -> wrap(em -> find(em, id)));
-    }
+		return supplyAsync(() -> wrap(em -> find(em, id)));
+	}
 
-    public CompletionStage<User> findByName(String name) {
+	public CompletionStage<User> findByName(String name)
+	{
 
-        return supplyAsync(() -> wrap(em -> find(em, name)));
-    }
+		return supplyAsync(() -> wrap(em -> find(em, name)));
+	}
 
-    public CompletionStage<User> update(User user) {
+	public CompletionStage<User> update(User user)
+	{
 
-        return supplyAsync(() -> wrap(em -> update(em, user)));
-    }
+		return supplyAsync(() -> wrap(em -> update(em, user)));
+	}
 
-    public CompletionStage<User> delete(Long id) {
+	public CompletionStage<User> delete(Long id)
+	{
 
-        return supplyAsync(() -> wrap(em -> delete(em, id)));
-    }
+		return supplyAsync(() -> wrap(em -> delete(em, id)));
+	}
 
-    private <T> T wrap(Function<EntityManager, T> function) {
+	private <T> T wrap(Function<EntityManager, T> function)
+	{
 
-        return jpaApi.withTransaction(function);
-    }
+		return jpaApi.withTransaction(function);
+	}
 
-    private User insert(EntityManager em, User user) {
-        em.persist(user);
-        return user;
-    }
+	private User insert(EntityManager em, User user)
+	{
+		em.persist(user);
+		return user;
+	}
 
-    private User update(EntityManager em, User user) {
-        em.merge(user);
-        return user;
-    }
+	private User update(EntityManager em, User user)
+	{
+		em.merge(user);
+		return user;
+	}
 
-    /**
-     * Deletes user with given id.
-     * First all sessions with a reference to this user have to be deleted
-     * in order to delete the user (or else Postgres-Exception fires).
-     */
-    private User delete(EntityManager em, Long id) {
-        User userToDelete = find(em, id);
-        Stream<Session> sessions = sessionRepository.findAll(em);
-        Stream<Session> sessionsFiltered = sessions.filter(session -> session.getUser_fk() == id);
-        sessionsFiltered.forEach(session -> sessionRepository.delete(em, session.getId()));
-        em.remove(userToDelete);
-        return userToDelete;
-    }
+	/**
+	 * Deletes user with given id.
+	 * First all sessions with a reference to this user have to be deleted
+	 * in order to delete the user (or else Postgres-Exception fires).
+	 */
+	private User delete(EntityManager em, Long id)
+	{
+		User userToDelete = find(em, id);
+		Stream<Session> sessions = sessionRepository.findAll(em);
+		Stream<Session> sessionsFiltered = sessions
+				.filter(session -> session.getUser_fk() == id);
+		sessionsFiltered.forEach(
+				session -> sessionRepository.delete(em, session.getId()));
+		em.remove(userToDelete);
+		return userToDelete;
+	}
 
-    private Stream<User> findAll(EntityManager em) {
-        // get ResultList from returned Query
-        List<User> Users = em.createQuery("SELECT u FROM users u", User.class).getResultList();
-        // Return ResultList as continuous Stream (asynchronous implementation needs streams)
-        return Users.stream();
-    }
+	private Stream<User> findAll(EntityManager em)
+	{
+		// get ResultList from returned Query
+		List<User> Users = em.createQuery("SELECT u FROM users u", User.class)
+				.getResultList();
+		// Return ResultList as continuous Stream (asynchronous implementation needs streams)
+		return Users.stream();
+	}
 
-    private User find(EntityManager em, Long id) {
-        return em.find(User.class, id);
-    }
+	private User find(EntityManager em, Long id)
+	{
+		return em.find(User.class, id);
+	}
 
-    private User find(EntityManager em, String name) {
-        String query = "SELECT u FROM users u WHERE username=\'" + name + "\'";
-        List<User> users = em.createQuery(query, User.class).getResultList();
-        return users.get(0) != null ? users.get(0) : null;
-    }
+	private User find(EntityManager em, String name)
+	{
+		String query = "SELECT u FROM users u WHERE username=\'" + name + "\'";
+		List<User> users = em.createQuery(query, User.class).getResultList();
+		if(users.isEmpty())
+		{
+			return null;
+		}
+		return users.get(0) != null ? users.get(0) : null;
+	}
 }
---
+++ public/javascripts/utils/registration.js
@@ -0,0 +1,137 @@
+
+// get elements from modal
+const submit = $("#registerSubmit");
+const userSignUp = $("#usernameSignUp");
+const passSignUp = $("#passwordSignUp");
+const passConfSignUp = $("#passwordSignUpConfirm");
+const userLogIn = $("#usernameLogIn");
+const passLogIn = $("#passwordLogIn");
+
+// add event-listeners to elements
+submit.on("click", submitForm);
+userSignUp.on("change", validateUserSignUp);
+passSignUp.on("change", validatePassSignUp);
+passConfSignUp.on("change", validatePassConfSignUp);
+userLogIn.on("change", validateUserLogIn);
+passLogIn.on("change", validatePassLogIn);
+
+
+
+function submitForm() {
+    let tabs = $(".tab-content").children();
+    let activeTab = tabs.filter(index => tabs[index].classList.contains("active"));
+    let id = activeTab[0].id;
+    switch (id) {
+        case "logIn":
+            if(validateUserLogIn() && validatePassLogIn()) {
+                let username = userLogIn.val();
+                let password = passLogIn.val();
+                let basic = "Basic " + username + ":" + password;
+                fetch('http://localhost:9000/api/users/check', {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                        "Authorization": basic
+                    }
+                }).then(response => {
+                    console.log(response.status);
+                    return response.status;
+                }).catch(err => console.log(err));
+            }
+            break;
+        case "signUp":
+            if( validatePassConfSignUp() &&
+                validatePassSignUp() &&
+                validateUserSignUp()) {
+
+            }
+            break;
+    }
+}
+
+function validateUserSignUp() {
+    // only accept alphanumeric signs starting with a letter
+    // all letters are lowercase
+    let regex = /^\w+$/;
+    let input = userSignUp.val().toLowerCase();
+    let invalidDiv = '<div id="invalidUSU" class="invalid-feedback">Only use alphanumeric sings</div>';
+    let nameShortDiv = '<div id="shortUSU" class="invalid-feedback">Name is too short</div>';
+    $('#invalidUSU').remove();
+    $('#shortUSU').remove();
+    userSignUp.removeClass("is-invalid");
+    if (input.length > 0 && input.length < 3) {
+        userSignUp.parent().append(nameShortDiv);
+        userSignUp.addClass("is-invalid");
+    }
+    if(input.length > 0 && !regex.test(input)) {
+        userSignUp.parent().append(invalidDiv);
+        userSignUp.addClass("is-invalid");
+    }
+    return (input.length > 3 && regex.test(input));
+}
+
+function validatePassSignUp() {
+    validatePassConfSignUp();
+    // only accept alphanumeric signs starting with a letter
+    // all letters are lowercase
+    let regex = /^\w+$/;
+    let input = passSignUp.val().toLowerCase();
+    let invalidDiv = '<div id="invalidPSU" class="invalid-feedback">Only use alphanumeric sings</div>';
+    let nameShortDiv = '<div id="shortPSU" class="invalid-feedback">Name is too short</div>';
+    $('#invalidPSU').remove();
+    $('#shortPSU').remove();
+    passSignUp.removeClass("is-invalid");
+    if (input.length > 0 && input.length < 3) {
+        passSignUp.parent().append(nameShortDiv);
+        passSignUp.addClass("is-invalid");
+    }
+    if(input.length > 0 && !regex.test(input)) {
+        passSignUp.parent().append(invalidDiv);
+        passSignUp.addClass("is-invalid");
+    }
+    return (input.length > 3 && regex.test(input));
+}
+
+function validatePassConfSignUp() {
+    // compare both password-inputs
+    let input = passConfSignUp.val().toLowerCase();
+    let inputPass = passSignUp.val().toLowerCase();
+    let notEqualDiv = '<div id="notEqualPCSU" class="invalid-feedback">Password does not match</div>';
+    $('#notEqualPCSU').remove();
+    passConfSignUp.removeClass("is-invalid");
+    if (input.length > 0 && input != inputPass) {
+        passConfSignUp.parent().append(notEqualDiv);
+        passConfSignUp.addClass("is-invalid");
+    }
+    return (input == inputPass);
+}
+
+function validateUserLogIn() {
+    // only accept alphanumeric signs starting with a letter
+    // all letters are lowercase
+    let regex = /^\w+$/;
+    let input = userLogIn.val().toLowerCase();
+    let invalidDiv = '<div id="invalidULI" class="invalid-feedback">Only use alphanumeric sings</div>';
+    $('#invalidULI').remove();
+    userLogIn.removeClass("is-invalid");
+    if(input.length > 0 && !regex.test(input)) {
+        userLogIn.parent().append(invalidDiv);
+        userLogIn.addClass("is-invalid");
+    }
+    return (regex.test(input));
+}
+
+function validatePassLogIn() {
+// only accept alphanumeric signs starting with a letter
+    // all letters are lowercase
+    let regex = /^\w+$/;
+    let input = passLogIn.val().toLowerCase();
+    let invalidDiv = '<div id="invalidPLI" class="invalid-feedback">Only use alphanumeric sings</div>';
+    $('#invalidPLI').remove();
+    passLogIn.removeClass("is-invalid");
+    if(input.length > 0 && !regex.test(input)) {
+        passLogIn.parent().append(invalidDiv);
+        passLogIn.addClass("is-invalid");
+    }
+    return (regex.test(input));
+}
